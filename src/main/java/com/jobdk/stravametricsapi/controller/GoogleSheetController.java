60d1038beac821e3896ab504940a98714bc77ef1
jobdk <johnybudnik@gmail.com>
2023-11-10 14:13:48 +0100

    Idea for accessing the goal kilometers

---
+++ src/main/java/com/jobdk/stravametricsapi/controller/GoogleSheetController.java
@@ -0,0 +1,12 @@
+package com.jobdk.stravametricsapi.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GoogleSheetController {
+    /* TODO: Idea
+    Creating goal kilometers in google sheet and access them via the api.
+    -> Why? Interested in the api.
+     */
+
+}
